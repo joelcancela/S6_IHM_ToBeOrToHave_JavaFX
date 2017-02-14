ef083b09b2b9e69b1666b01e81b3fff96841f79b
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2017-02-14 21:47:40 +0100

    Trying to import an FXML file into a tab

--- enseigne/src/main/java/polytech/si3/ihm/tobeortohave/MainWindowController.java
+++ enseigne/src/main/java/polytech/si3/ihm/tobeortohave/MainWindowController.java
@@ -3,9 +3,12 @@ package polytech.si3.ihm.tobeortohave;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.layout.Pane;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,17 +18,22 @@ public class MainWindowController {
     private static final Logger log = LoggerFactory.getLogger(MainWindowController.class);
 
     @FXML
-    private TabPane tabPaneMain;
-
+    public Tab tab0;
     @FXML
-    private TabPane tabPaneMagasins;
-
+    public Tab tab1;
     @FXML
-    private TabPane tabPaneProduits;
+    public Tab tab2;
+
 
     @FXML
     public void initialize() {
-
+        try {
+            String fxmlFile = "/fxml/homeWindow.fxml";
+            FXMLLoader loader2 = new FXMLLoader();
+            this.tab0.setContent(loader2.load(getClass().getResourceAsStream(fxmlFile)));
+        } catch (IOException ie) {
+           ie.printStackTrace();
+        }
     }
 
     public void storeTabSelected(Event event) {
