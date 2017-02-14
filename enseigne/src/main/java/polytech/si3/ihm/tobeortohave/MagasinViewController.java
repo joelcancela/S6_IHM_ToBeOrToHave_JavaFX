852e9fd69649da146a639ce74e68574778bacdda
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2017-02-15 00:55:20 +0100

    Stores selected by default & stores redesigned

--- enseigne/src/main/java/polytech/si3/ihm/tobeortohave/MagasinViewController.java
+++ enseigne/src/main/java/polytech/si3/ihm/tobeortohave/MagasinViewController.java
@@ -2,7 +2,9 @@ package polytech.si3.ihm.tobeortohave;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TabPane;
 
@@ -23,6 +25,9 @@ public class MagasinViewController {
     public ListView listStores;
 
     @FXML
+    public Button validButton;
+
+    @FXML
     public void initialize() {
         ObservableList<String> regions = FXCollections.observableArrayList("Auvergne-Rhône-Alpes",
                                                                          "Bourgogne-Franche-Comté",
@@ -38,12 +43,23 @@ public class MagasinViewController {
                                                                          "Pays de la Loire",
                                                                          "Provence-Alpes-Côte d'Azur");
         this.listRegion.setItems(regions);
+        this.listRegion.getSelectionModel().select(0);
 
         ObservableList<String> cities = FXCollections.observableArrayList("Ville 1", "Ville 2", "Ville 3", "Ville 4", "Ville 5", "Ville 6", "Ville 7", "Ville 8", "Ville 9","Ville 10");
         this.listVille.setItems(cities);
+        this.listVille.getSelectionModel().select(0);
 
         ObservableList<String> stores = FXCollections.observableArrayList("Magasin 1", "Magasin 2", "Magasin 3", "Magasin 4", "Magasin 5", "Magasin 6", "Magasin 7", "Magasin 8", "Magasin 9", "Magasin 10");
         this.listStores.setItems(stores);
+        this.listStores.getSelectionModel().select(0);
+
+    }
+
+    public void validButtonClicked(ActionEvent actionEvent) {
+            String region = this.listRegion.getSelectionModel().getSelectedItem().toString();
+            String ville = this.listVille.getSelectionModel().getSelectedItem().toString();
+            String magasin = this.listStores.getSelectionModel().getSelectedItem().toString();
+            System.out.println("Validation Clicked : " + region + " : " + ville + " : " + magasin);
     }
 }
 
