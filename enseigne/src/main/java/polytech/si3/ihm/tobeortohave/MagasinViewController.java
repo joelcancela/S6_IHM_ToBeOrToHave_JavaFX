8cf1485c7b918e8a024023108cb506eb9c28d485
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2017-02-14 23:52:25 +0100

    Cities & Stores examples added

--- enseigne/src/main/java/polytech/si3/ihm/tobeortohave/MagasinViewController.java
+++ enseigne/src/main/java/polytech/si3/ihm/tobeortohave/MagasinViewController.java
@@ -17,10 +17,12 @@ public class MagasinViewController {
 
     @FXML
     public ListView listRegion;
+    public ListView listVille;
+    public ListView listStores;
 
     @FXML
     public void initialize() {
-        ObservableList<String> items = FXCollections.observableArrayList("Auvergne-Rhône-Alpes",
+        ObservableList<String> regions = FXCollections.observableArrayList("Auvergne-Rhône-Alpes",
                                                                          "Bourgogne-Franche-Comté",
                                                                          "Bretagne",
                                                                          "Centre-Val de Loire",
@@ -33,7 +35,13 @@ public class MagasinViewController {
                                                                          "Occitanie",
                                                                          "Pays de la Loire",
                                                                          "Provence-Alpes-Côte d'Azur");
-        this.listRegion.setItems(items);
+        this.listRegion.setItems(regions);
+
+        ObservableList<String> cities = FXCollections.observableArrayList("Ville 1", "Ville 2", "Ville 3", "Ville 4", "Ville 5", "Ville 6", "Ville 7", "Ville 8", "Ville 9","Ville 10");
+        this.listVille.setItems(cities);
+
+        ObservableList<String> stores = FXCollections.observableArrayList("Magasin 1", "Magasin 2", "Magasin 3", "Magasin 4", "Magasin 5", "Magasin 6", "Magasin 7", "Magasin 8", "Magasin 9", "Magasin 10");
+        this.listStores.setItems(stores);
     }
 }
 
