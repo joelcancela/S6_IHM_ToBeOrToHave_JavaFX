a1944b41ae97572c565f65fc3021c48f6c4986f8
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2017-02-14 22:51:49 +0100

    French régions added

--- enseigne/src/main/java/polytech/si3/ihm/tobeortohave/MagasinViewController.java
+++ enseigne/src/main/java/polytech/si3/ihm/tobeortohave/MagasinViewController.java
@@ -1,7 +1,12 @@
 package polytech.si3.ihm.tobeortohave;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TabPane;
+
+import java.util.Observable;
 
 /**
  * Class x
@@ -11,8 +16,24 @@ import javafx.scene.control.TabPane;
 public class MagasinViewController {
 
     @FXML
-    public void initialize() {
+    public ListView listRegion;
 
+    @FXML
+    public void initialize() {
+        ObservableList<String> items = FXCollections.observableArrayList("Auvergne-Rhône-Alpes",
+                                                                         "Bourgogne-Franche-Comté",
+                                                                         "Bretagne",
+                                                                         "Centre-Val de Loire",
+                                                                         "Corse",
+                                                                         "Grand Est",
+                                                                         "Hauts-de-France",
+                                                                         "Île-de-France",
+                                                                         "Normandie",
+                                                                         "Nouvelle-Aquitaine",
+                                                                         "Occitanie",
+                                                                         "Pays de la Loire",
+                                                                         "Provence-Alpes-Côte d'Azur");
+        this.listRegion.setItems(items);
     }
 }
 
