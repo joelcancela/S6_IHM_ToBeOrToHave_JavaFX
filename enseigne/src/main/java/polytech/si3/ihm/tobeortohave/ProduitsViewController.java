746c8aa302448e7e5ad236361123d3349be4e860
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2017-02-15 01:52:26 +0100

    Product grid with image

--- enseigne/src/main/java/polytech/si3/ihm/tobeortohave/ProduitsViewController.java
+++ enseigne/src/main/java/polytech/si3/ihm/tobeortohave/ProduitsViewController.java
@@ -1,6 +1,11 @@
 package polytech.si3.ihm.tobeortohave;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import net.miginfocom.layout.Grid;
 
 /**
  * Class x
@@ -8,8 +13,20 @@ import javafx.fxml.FXML;
  * @author Joël CANCELA VAZ
  */
 public class ProduitsViewController {
+    public GridPane productGrid;
+
     @FXML
     public void initialize() {
 
+    }
+
+    public void pictureClick(MouseEvent mouseEvent) {
+        for (Node node : this.productGrid.getChildren()) {
+            if (node instanceof ImageView) {
+                if( node.getBoundsInParent().contains(mouseEvent.getSceneX(),  mouseEvent.getSceneY())) {
+                    System.out.println( "Node: " + node + " at " + GridPane.getRowIndex( node) + "/" + GridPane.getColumnIndex( node));
+                }
+            }
+        }
     }
 }
