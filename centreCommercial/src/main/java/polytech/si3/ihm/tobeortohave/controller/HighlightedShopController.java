4e1304207391cf1cc11492b9c8fb6dc12cbe3b0e
Marc J <marcjourdes@free.fr>
2017-03-07 23:52:24 +0100

    Centre Commercial Init controllers

---
+++ centreCommercial/src/main/java/polytech/si3/ihm/tobeortohave/controller/HighlightedShopController.java
@@ -0,0 +1,24 @@
+package polytech.si3.ihm.tobeortohave.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+/**
+ * Created by MARC-PC on 07/03/2017.
+ */
+public class HighlightedShopController {
+
+    @FXML
+    public ImageView brandImage;
+
+    @FXML
+    public Label shopName;
+
+    @FXML
+    public Label shopDescription;
+
+    @FXML
+    public Button moreButton;
+}
