024d184d537a26d7801327f696ae108ec879c6de
Kariamoss <jehanmilleret@gmail.com>
2017-03-08 09:05:32 +0100

    Add init highlighted shops to controller

--- centreCommercial/src/main/java/polytech/si3/ihm/tobeortohave/controller/HighlightedShopController.java
+++ centreCommercial/src/main/java/polytech/si3/ihm/tobeortohave/controller/HighlightedShopController.java
@@ -3,7 +3,9 @@ package polytech.si3.ihm.tobeortohave.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import polytech.si3.ihm.tobeortohave.model.Shop;
 
 /**
  * Created by MARC-PC on 07/03/2017.
@@ -21,4 +23,10 @@ public class HighlightedShopController {
 
     @FXML
     public Button moreButton;
+
+    public void initHighlightedShop(Shop shop){
+        brandImage.setImage(new Image(shop.getLogoPath()));
+        shopName.setText(shop.getName());
+        shopDescription.setText(shop.getDescription());
+    }
 }
