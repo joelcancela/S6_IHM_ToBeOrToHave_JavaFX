a7b73f98a8e940f1ee5c2571441668bc5babf47e
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2017-03-12 13:41:17 +0100

    Edited homeview enseigne

---
+++ enseigne/src/main/java/polytech/si3/ihm/tobeortohave/InnerHomeWindowController.java
@@ -0,0 +1,73 @@
+package polytech.si3.ihm.tobeortohave;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import polytech.si3.ihm.tobeortohave.model.JSONReader;
+import polytech.si3.ihm.tobeortohave.model.Produit;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * {@code InnerHomeWindowController} is [desc]
+ * <p>
+ * [descSuite]
+ *
+ * @author Nassim BOUNOUAS
+ * @author Joël CANCELA VAZ
+ */
+public class InnerHomeWindowController {
+	@FXML
+	private GridPane homeproducts;
+
+	private JSONReader jsonReader;
+
+
+	@FXML
+	public void initialize() {
+		jsonReader = new JSONReader();
+		jsonReader.parse();
+		initializeProducts();
+	}
+
+	private void initializeProducts() {
+		List<Produit> produitList = jsonReader.getProducts();
+		int x=0;
+		int y=0;
+		for (Produit produit : produitList) {
+			Node produitNode = createProductNode(produit);
+			homeproducts.add(produitNode,x++,y);
+			if(x==6){
+				x=0;
+				y=y+1;
+			}
+		}
+	}
+
+	private Node createProductNode(Produit p){
+		VBox vBox = new VBox();
+
+		//Picture
+		File file = new File("common/src/main/resources/images/products/"+p.getPicture());
+		Image image = new Image(file.toURI().toString());
+		ImageView imageView = new ImageView(image);
+		imageView.setPreserveRatio(true);
+		imageView.setFitHeight(100.0);
+		vBox.getChildren().add(imageView);
+
+		//Text
+		Label label = new Label(p.getName());
+		label.setFont(Font.font("System", FontWeight.NORMAL, 16));
+		vBox.getChildren().add(label);
+
+		return vBox;
+	}
+
+}
--- enseigne/src/main/java/polytech/si3/ihm/tobeortohave/MainWindowController.java
+++ enseigne/src/main/java/polytech/si3/ihm/tobeortohave/MainWindowController.java
@@ -4,59 +4,57 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 public class MainWindowController {
-    private static final Logger log = LoggerFactory.getLogger(MainWindowController.class);
+	private static final Logger log = LoggerFactory.getLogger(MainWindowController.class);
 
-    @FXML
-    public Tab tab0;
-    @FXML
-    public Tab tab1;
-    @FXML
-    public Tab tab2;
+	@FXML
+	public Tab tab0;
+	@FXML
+	public Tab tab1;
+	@FXML
+	public Tab tab2;
 
 
-    @FXML
-    public void initialize() {
-        initializeTab("homeWindow", this.tab0);
-        initializeTab("magasinView", this.tab1);
-        initializeTab("produitsView", this.tab2);
-    }
+	@FXML
+	public void initialize() {
+		initializeTab("homeWindow", this.tab0);
+		initializeTab("magasinView", this.tab1);
+		initializeTab("produitsView", this.tab2);
+	}
 
-    private void initializeTab(String fxmlName, Tab tab) {
-        try {
-            String fxmlFile = "/fxml/" + fxmlName + ".fxml";
-            FXMLLoader loader2 = new FXMLLoader();
-            Node node = loader2.load(getClass().getResourceAsStream(fxmlFile));
-            AnchorPane.setTopAnchor(node,0.0);
-            AnchorPane.setLeftAnchor(node,0.0);
-            AnchorPane.setBottomAnchor(node,0.0);
-            AnchorPane.setRightAnchor(node,0.0);
-            tab.setContent(node);
-        } catch (IOException ie) {
-            ie.printStackTrace();
-        }
-    }
+	private void initializeTab(String fxmlName, Tab tab) {
+		try {
+			String fxmlFile = "/fxml/" + fxmlName + ".fxml";
+			FXMLLoader loader2 = new FXMLLoader();
+			Node node = loader2.load(getClass().getResourceAsStream(fxmlFile));
+			AnchorPane.setTopAnchor(node, 0.0);
+			AnchorPane.setLeftAnchor(node, 0.0);
+			AnchorPane.setBottomAnchor(node, 0.0);
+			AnchorPane.setRightAnchor(node, 0.0);
+			tab.setContent(node);
+		} catch (IOException ie) {
+			ie.printStackTrace();
+		}
+	}
 
-    public void storeTabSelected(Event event) {
-        System.out.println("Store selected");
-    }
 
-    public void homeTabSelected(Event event) {
-        System.out.println("Home selected");
-    }
 
-    public void productTabSelected(Event event) {
-        System.out.println("Product selected");
-    }
+	public void storeTabSelected(Event event) {
+		System.out.println("Store selected");
+	}
+
+	public void homeTabSelected(Event event) {
+		System.out.println("Home selected");
+	}
+
+	public void productTabSelected(Event event) {
+		System.out.println("Product selected");
+	}
 }
