e623b3fcce74474209d8c8ce63dc6b156e2b146a
Kariamoss <jehanmilleret@gmail.com>
2017-03-12 14:57:37 +0100

    Infos Sup visu

---
+++ centreCommercial/src/main/java/polytech/si3/ihm/tobeortohave/controller/InfosSupController.java
@@ -0,0 +1,21 @@
+package polytech.si3.ihm.tobeortohave.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.image.ImageView;
+
+/**
+ * Created by Jehan on 12/03/2017.
+ */
+public class InfosSupController {
+
+    @FXML
+    public ImageView map;
+
+    @FXML
+    public BarChart age;
+
+    @FXML
+    public PieChart profil;
+}
