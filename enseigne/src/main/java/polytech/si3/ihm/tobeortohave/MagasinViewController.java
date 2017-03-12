0dfee80243a4681fc70653b02d23dc51efdc3eb3
Joël CANCELA VAZ <joel.cancelavaz@gmail.com>
2017-03-12 12:01:05 +0100

    updated magasinView enseigne + database.json

--- enseigne/src/main/java/polytech/si3/ihm/tobeortohave/MagasinViewController.java
+++ enseigne/src/main/java/polytech/si3/ihm/tobeortohave/MagasinViewController.java
@@ -1,72 +1,104 @@
 package polytech.si3.ihm.tobeortohave;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.*;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import polytech.si3.ihm.tobeortohave.model.Enseigne;
+import polytech.si3.ihm.tobeortohave.model.JSONReader;
+import polytech.si3.ihm.tobeortohave.model.Magasin;
 
-import java.util.Observable;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
 
-/**
- * Class x
- *
- * @author Joël CANCELA VAZ
- */
-public class MagasinViewController {
 
-    @FXML
-    public ListView listRegion;
-    @FXML
-    public ListView listVille;
-    @FXML
-    public ListView listStores;
+public class MagasinViewController implements Initializable, MapComponentInitializedListener {
 
-    @FXML
-    public Button validButton;
+	@FXML
+	private VBox vbox;
 
-    @FXML
-    public void initialize() {
-        ObservableList<String> regions = FXCollections.observableArrayList("Auvergne-Rhône-Alpes",
-                                                                         "Bourgogne-Franche-Comté",
-                                                                         "Bretagne",
-                                                                         "Centre-Val de Loire",
-                                                                         "Corse",
-                                                                         "Grand Est",
-                                                                         "Hauts-de-France",
-                                                                         "Île-de-France",
-                                                                         "Normandie",
-                                                                         "Nouvelle-Aquitaine",
-                                                                         "Occitanie",
-                                                                         "Pays de la Loire",
-                                                                         "Provence-Alpes-Côte d'Azur");
-        this.listRegion.setItems(regions);
-        this.listRegion.getSelectionModel().select(0);
+	@FXML
+	private GoogleMapView mapView;
 
-        ObservableList<String> cities = FXCollections.observableArrayList("Ville 1", "Ville 2", "Ville 3", "Ville 4", "Ville 5", "Ville 6", "Ville 7", "Ville 8", "Ville 9","Ville 10");
-        this.listVille.setItems(cities);
-        this.listVille.getSelectionModel().select(0);
+	private GoogleMap map;
 
-        ObservableList<String> stores = FXCollections.observableArrayList("Magasin 1", "Magasin 2", "Magasin 3", "Magasin 4", "Magasin 5", "Magasin 6", "Magasin 7", "Magasin 8", "Magasin 9", "Magasin 10");
-        this.listStores.setItems(stores);
-        this.listStores.getSelectionModel().select(0);
+	private JSONReader jsonReader;
 
-    }
+	public void initialize(URL url, ResourceBundle rb) {
+		jsonReader = new JSONReader();
+		jsonReader.parse();
+		mapView.addMapInializedListener(this);
+	}
 
-    public void validButtonClicked(ActionEvent actionEvent) {
-            String region = this.listRegion.getSelectionModel().getSelectedItem().toString();
-            String ville = this.listVille.getSelectionModel().getSelectedItem().toString();
-            String magasin = this.listStores.getSelectionModel().getSelectedItem().toString();
-            System.out.println("Validation Clicked : " + region + " : " + ville + " : " + magasin);
 
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Information Magasin");
-            alert.setHeaderText("Votre magasin : " + magasin);
-            alert.setContentText("00 Addresse\n06xxx " + ville + "\n" + region);
-            alert.showAndWait();
-    }
+	public void mapInitialized() {
+		List<Magasin> magasinList = jsonReader.getStores();
+		List<Magasin> magasinsTboth = Enseigne.findMagasinByBrand(magasinList, "ToBeOrToHave");
+		Magasin magasinDefault = magasinList.get(0);
+		LatLong defaultShopLocation = new LatLong(magasinDefault.getLatitude(), magasinDefault.getLongitude());
+
+
+		//Set the initial properties of the map.
+		MapOptions mapOptions = new MapOptions();
+
+		mapOptions.center(defaultShopLocation)
+				.mapType(MapTypeIdEnum.ROADMAP)
+				.overviewMapControl(false)
+				.panControl(false)
+				.mapTypeControl(false)
+				.rotateControl(false)
+				.scaleControl(false)
+				.streetViewControl(false)
+				.zoomControl(false)
+				.zoom(19)
+				.styleString("[{featureType: \"poi\",stylers: [{ visibility: \"off\" }]}]");
+
+		map = mapView.createMap(mapOptions);
+
+
+
+		vbox.setSpacing(10.0);
+		for (int i = 0; i < magasinList.size(); i++) {
+			Magasin magasin = magasinList.get(i);
+			Button button = new Button(magasin.getAddress());
+			button.setStyle("-fx-font-size: 20px;");
+			button.addEventHandler(MouseEvent.MOUSE_CLICKED,
+					e -> {
+						map.clearMarkers();
+						LatLong latLong = new LatLong(magasin.getLatitude(), magasin.getLongitude());
+						//Add markers to the map
+						MarkerOptions markerOptions = new MarkerOptions();
+						markerOptions.position(latLong);
+
+
+						Marker myShopMarker = new Marker(markerOptions);
+
+
+						map.addMarker(myShopMarker);
+
+
+						InfoWindowOptions infoWindowOptions = new InfoWindowOptions();
+						infoWindowOptions.content("<h2>ToBeOrToHave "+magasin.getAddress().toUpperCase()+"</h2>"
+								+ "Telephone: " + magasin.getPhoneNumber() + "<br>"
+								+ "Web: " + magasin.getWebAddress());
+
+						InfoWindow InfoWindow = new InfoWindow(infoWindowOptions);
+						InfoWindow.open(map, myShopMarker);
+
+						map.setCenter(latLong);
+					});
+			if (i == 0) {
+				button.fireEvent(new MouseEvent(MouseEvent.MOUSE_CLICKED, 1, 2, 3, 4, MouseButton.PRIMARY, 5, true, true, true, true, true, true, true, true, true, true, null));
+			}
+			vbox.getChildren().add(button);
+		}
+
+
+	}
 }
-
