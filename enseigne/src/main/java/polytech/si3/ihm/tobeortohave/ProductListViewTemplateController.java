51e8052bdc420463e76e5db22949282e6902e126
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2017-03-12 15:52:10 +0100

    Trying to implement a listView

---
+++ enseigne/src/main/java/polytech/si3/ihm/tobeortohave/ProductListViewTemplateController.java
@@ -0,0 +1,26 @@
+package polytech.si3.ihm.tobeortohave;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.*;
+import polytech.si3.ihm.tobeortohave.model.Produit;
+
+import java.io.File;
+
+/**
+ * Created by Galih on 12/03/2017.
+ */
+public class ProductListViewTemplateController {
+
+    @FXML
+    public ImageView productView;
+    @FXML
+    public Label productName;
+
+    public void initProduct(Produit prod) {
+        this.productName.setText(prod.getName());
+        File file = new File("common/src/main/resources/images/products/"+prod.getPicture());
+        Image image = new javafx.scene.image.Image(file.toURI().toString());
+        this.productView.setImage(image);
+    }
+}
--- enseigne/src/main/java/polytech/si3/ihm/tobeortohave/ProduitsViewController.java
+++ enseigne/src/main/java/polytech/si3/ihm/tobeortohave/ProduitsViewController.java
@@ -1,14 +1,23 @@
 package polytech.si3.ihm.tobeortohave;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.util.Callback;
 import jdk.nashorn.internal.parser.JSONParser;
 import net.miginfocom.layout.Grid;
 import org.json.JSONObject;
 import polytech.si3.ihm.tobeortohave.model.JSONReader;
+import polytech.si3.ihm.tobeortohave.model.Produit;
+
+import java.io.IOException;
+
 
 /**
  * Class x
@@ -17,14 +26,43 @@ import polytech.si3.ihm.tobeortohave.model.JSONReader;
  */
 public class ProduitsViewController {
     public GridPane productGrid;
-    public GridPane bestSellerGrid;
     private JSONReader jsonReader;
 
+    public GridPane bestSellerGrid;
+    public ListView listViewPromotion;
 
     @FXML
     public void initialize() {
         jsonReader = new JSONReader();
         jsonReader.parse();
+        //listViewPromotion.setItems();
+        listViewPromotion.setCellFactory(
+                new Callback<ListView<Produit>, ListCell<Produit>>() {
+                    @Override
+                    public ListCell<Produit> call(ListView<Produit> param) {
+                        return new ListCell<Produit>() {
+                            @Override
+                            protected void updateItem(Produit item, boolean empty) {
+                                super.updateItem(item, empty);
+
+                                if (item != null) {
+                                    // Load fxml file for this internship
+                                    try {
+                                        String fxmlFile = "/fxml/productListViewTemplate.fxml";
+                                        FXMLLoader loader = new FXMLLoader();
+                                        Parent listElement = loader.load(getClass().getResourceAsStream(fxmlFile));
+                                        ((ProductListViewTemplateController) loader.getController()).initProduct(item);
+                                        // Display content of the fxml file
+                                        this.setGraphic(listElement);
+                                    } catch (IOException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        };
+                    }
+                }
+        );
     }
 
     public void pictureClick(MouseEvent mouseEvent) {
