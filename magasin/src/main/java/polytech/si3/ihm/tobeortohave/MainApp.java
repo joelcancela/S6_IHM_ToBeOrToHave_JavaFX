6aacf1eafe609af1454e79545489327151e3abb0
Nikita Rousseau <nikita.rousseau@etu.unice.fr>
2017-03-14 20:39:03 +0100

    Style modules ready

--- magasin/src/main/java/polytech/si3/ihm/tobeortohave/MainApp.java
+++ magasin/src/main/java/polytech/si3/ihm/tobeortohave/MainApp.java
@@ -13,8 +13,21 @@ public class MainApp extends Application {
     private static final Logger log = LoggerFactory.getLogger(MainApp.class);
 
     private Stage primaryStage;
+    private static boolean bsTheme = false;
+    private static boolean darkTheme = false;
 
     public static void main(String[] args) throws Exception {
+
+        // read args
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("dark")) {
+                darkTheme = true;
+            }
+            if (args[i].equals("bs")) {
+                bsTheme = true;
+            }
+        }
+
         launch(args);
     }
 
@@ -23,15 +36,32 @@ public class MainApp extends Application {
         this.primaryStage = stage;
 
         String fxmlFile = "/fxml/common.fxml";
+
         FXMLLoader loader = new FXMLLoader();
+
         Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
 
-        Scene scene = new Scene(rootNode);
-        scene.getStylesheets().add("/styles/styles.css");
+        // select theme
+        if (MainApp.darkTheme) {
+            loadStyle(rootNode, "dark-metro.css");
+        }
+        else if (MainApp.bsTheme) {
+            loadStyle(rootNode, "bs.css");
+        }
+        else {
+            loadStyle(rootNode, "common.css");
+        }
 
-        this.primaryStage.setTitle("ToBeOrToHave - Magasin");
+        Scene scene = new Scene(rootNode);
+
+        this.primaryStage.setTitle("ToBeOrToHave");
         this.primaryStage.setResizable(false);
         this.primaryStage.setScene(scene);
         this.primaryStage.show();
+    }
+
+    private void loadStyle(Parent node, String filename) {
+        node.getStylesheets().clear();
+        node.getStylesheets().add(getClass().getResource("/styles/" + filename).toExternalForm());
     }
 }
