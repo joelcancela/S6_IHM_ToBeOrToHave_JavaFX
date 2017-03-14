25927c43350a52a421ec2317f5acd71c2c138ca6
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2017-03-14 04:06:09 +0100

    Controller for template Product

--- enseigne/src/main/java/polytech/si3/ihm/tobeortohave/administration/ProductAdminTabController.java
+++ enseigne/src/main/java/polytech/si3/ihm/tobeortohave/administration/ProductAdminTabController.java
@@ -4,4 +4,8 @@ package polytech.si3.ihm.tobeortohave.administration;
  * Created by Galih on 14/03/2017.
  */
 public class ProductAdminTabController {
+
+    public void initialize() {
+
+    }
 }
--- enseigne/src/main/java/polytech/si3/ihm/tobeortohave/administration/productAdminListViewTemplateController.java
+++ enseigne/src/main/java/polytech/si3/ihm/tobeortohave/administration/productAdminListViewTemplateController.java
@@ -3,9 +3,5 @@ package polytech.si3.ihm.tobeortohave.administration;
 /**
  * Created by Galih on 14/03/2017.
  */
-public class HomeAdminViewController {
-
-    public void initialize() {
-
-    }
+public class productAdminListViewTemplateController {
 }
